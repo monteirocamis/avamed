9e2a22da82e4e842e023d7463f35b6df2ea0ffbf
Pilar Leminski Veiga <pilar.leminski.veiga@avanade.com>
2022-12-20 11:51:00 -0300

    DTOs created

--- Avamed/Dto/AgendamentoConfiguracao.cs
+++ Avamed/Dto/AgendamentoConfiguracao.cs
@@ -1,4 +1,4 @@
-﻿using Avamed.Database;
+using Avamed.Database;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
@@ -6,20 +6,17 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Avamed.Dto
-{
-    public class AgendamentoDto
-    {
-        public int IdAgendamento { get; set; }
+namespace Avamed.Dto {
+    public class AgendamentoConfiguracao {
+        public int IdConfiguracao { get; set; }
         public int IdHospital { get; set; }
         public int IdEspecialidade { get; set; }
         public int IdProfissional { get; set; }
-        public DateTime DataHoraAgendamento { get; set; }
-        public int IdBeneficiario { get; set; }
-        public bool Ativo { get; set; }
-        public virtual Beneficiario IdBeneficiarioNavigation { get; set; } = null!;
+        public DateTime DataHoraInicioAtendimento { get; set; }
+        public DateTime DataHoraFinalAtendimento { get; set; }
         public virtual Especialidade IdEspecialidadeNavigation { get; set; } = null!;
         public virtual Hospital IdHospitalNavigation { get; set; } = null!;
         public virtual Profissional IdProfissionalNavigation { get; set; } = null!;
+
     }
 }
---
+++ Avamed/Dto/Profissional.cs
@@ -0,0 +1,22 @@
+using Avamed.Database;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Avamed.Dto;
+
+[Table("Profissional")]
+public partial class Profissional
+{
+    public int IdProfissional { get; set; }
+    public string Nome { get; set; } = null!;
+    public string? Telefone { get; set; }
+    public string? Endereço { get; set; }
+    public bool Ativo { get; set; }
+    public virtual ICollection<AgendamentoConfiguracao> AgendamentoConfiguracaos { get; } = new List<AgendamentoConfiguracao>();
+    public virtual ICollection<Agendamento> Agendamentos { get; } = new List<Agendamento>();
+    public virtual ICollection<DadosBancario> DadosBancarios { get; } = new List<DadosBancario>();
+}
