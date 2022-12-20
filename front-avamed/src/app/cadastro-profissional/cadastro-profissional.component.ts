b0017f1ab6c2698f30de2029a9c6064711e89bea
Camila Monteiro <monteiro.camis@gmail.com>
2022-12-20 12:51:24 -0300

    rotas organizadas

---
+++ front-avamed/src/app/cadastro-profissional/cadastro-profissional.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-profissional',
+  templateUrl: './cadastro-profissional.component.html',
+  styleUrls: ['./cadastro-profissional.component.css']
+})
+export class CadastroProfissionalComponent {
+
+}
