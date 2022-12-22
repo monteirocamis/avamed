fa1101b7961554d25bbf59b2d075ccf81a060799
Camila Monteiro <monteiro.camis@gmail.com>
2022-12-22 15:29:25 -0300

    ddd

--- front-avamed/src/app/models/placeholder.model.ts
+++
@@ -1,9 +0,0 @@
-export class Dados{
-   public idAgendamento: number;
-   public idHospital: number;
-  public idEspecialidade: number;
-  public idProfissional: number;
-  public dataHoraAgendamento: Date;
-  public idBeneficiario: number;
-  public ativo: boolean;
-}
